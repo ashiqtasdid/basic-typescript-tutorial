5e6dc6e482fdd72a46c258c7c094f5663bcae16b
AshiqTasdid <admin@ashiqthedev.com>
2023-07-10 18:40:41 +0600

    [+] Added Datatypes.ts

---
+++ datatypes.ts
@@ -0,0 +1,24 @@
+// In this tutorial, we will be using multiple types.
+
+// We have to define the types first
+
+// for string 
+
+let Name: string = 'Ashiq';
+// for number
+
+let age: number = 18; // we can also use float
+let currentAge: number = 18.5;
+
+// for boolean
+
+let isAlive: boolean = true;
+
+// for array
+
+let hobbies: string[] = ['Coding', 'Gaming', 'Streaming'];
+let money: number[] = [20, 30, 2];
+
+// for two types of data
+
+let mixed: (string | number)[] = ['Ashiq', 18, 20, 'Mars'];
